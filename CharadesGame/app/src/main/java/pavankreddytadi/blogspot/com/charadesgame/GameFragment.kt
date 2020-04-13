ef7e332af3e334a051c75a9333bd411899352f2f
Pavan Kumar <pavankumart46@gmail.com>
2020-04-13 12:47:04 +0530

    Added

--- CharadesGame/app/src/main/java/pavankreddytadi/blogspot/com/charadesgame/GameFragment.kt
+++ CharadesGame/app/src/main/java/pavankreddytadi/blogspot/com/charadesgame/GameFragment.kt
@@ -1,12 +1,14 @@
 package pavankreddytadi.blogspot.com.charadesgame
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 /**
  * A simple [Fragment] subclass.
@@ -15,9 +17,7 @@ class GameFragment : Fragment() {
 
     lateinit var score_tv : TextView
     lateinit var word_tv : TextView
-    var score = 0
-    var words = ""
-    var wordslist : MutableList<String> = mutableListOf()
+    lateinit var gameViewModel: GameViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,26 +25,34 @@ class GameFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_game, container, false)
+        gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        Log.i("GameFragment","GameViewModel is initialized")
         score_tv = v.findViewById(R.id.tv_score)
         word_tv = v.findViewById(R.id.tv_word)
         val gotit = v.findViewById<Button>(R.id.btn_gotit)
         val skip = v.findViewById<Button>(R.id.btn_skip)
         val endgame = v.findViewById<Button>(R.id.btn_endgame)
-        resetWords()
-        initViews()
+
+        if(gameViewModel.words!="")
+        {
+            word_tv.text = gameViewModel.words
+            score_tv.text = gameViewModel.score.toString()
+        }
+        else{
+            gameViewModel.resetWords()
+            initViews()
+            gameViewModel.resetWords()
+        }
+
         gotit.setOnClickListener {
-            if(score<10){
-                score++
-            }
-            score_tv.text = score.toString()
+            gameViewModel.updatePositiveScore()
+            score_tv.text = gameViewModel.score.toString()
             initViews()
         }
 
         skip.setOnClickListener {
-            if(score>-10){
-                score--
-            }
-            score_tv.text = score.toString()
+            gameViewModel.updateNegativeScore()
+            score_tv.text = gameViewModel.score.toString()
             initViews()
         }
 
@@ -52,18 +60,10 @@ class GameFragment : Fragment() {
     }
 
     private fun initViews() {
-        if(wordslist.isEmpty()){
-            words = "ALL WORDS ARE COMPLETE"
-        }
-        else{
-            words = wordslist.removeAt(0)
-        }
-        word_tv.text = words
+        gameViewModel.selectWords()
+        word_tv.text = gameViewModel.words
     }
 
-    fun resetWords(){
-        wordslist = mutableListOf("Duck","Dog","fox","chocolate","table","chair","Mouse","Cat","Elephant","Coat")
-        wordslist.shuffle()
-    }
+
 
 }
